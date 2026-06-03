676eab5d18de2d79514b6a5272f19b62470b97aa
Subhankar Patra <subhankarpatra258@gmail.com>
2026-06-03 21:16:10 +0530

    chore: remove unused Flutter codebase and CI workflow

--- client/android/app/src/main/kotlin/com/aura/chat/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.aura.chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- client/test_script.dart
+++
@@ -1,4 +0,0 @@
-import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
-void main() {
-  print(SearchViewConfig().customSearchView);
-}
